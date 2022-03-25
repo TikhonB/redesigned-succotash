88f46857af330c921bcec266e699498be4d70a8d
TikhonB <tishka.bogdanov@gmail.com>
2022-03-25 22:58:57 +0300

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,5 +15,29 @@ public class Main {
                     Education.values()[new Random().nextInt(Education.values().length)])
             );
         }
+        long count = persons.stream() // первый метод вывода кол-ва несовершеннолетних
+                .filter(x -> x.getAge() < 18)
+                .count();
+        System.out.println(count);
+
+        List<String> conscript = persons.stream() // второй метод получения призывников
+                .filter(age -> age.getAge() > 18 && age.getAge() < 27)
+                .filter(sex -> sex.getSex() != Sex.WOMAN)
+                .map(value -> value.getFamily())
+                .collect(Collectors.toList());
+        System.out.println(conscript);
+
+        List<Person> performance = persons.stream() // третий метод на вывод работоспособных людей
+                .filter(x -> x.getEducation() == Education.HIGHER)
+                .filter(y -> y.getAge() > 18)
+                .filter(z -> z.getSex() == Sex.MAN ?
+                        z.getAge() < 65 :
+                        z.getAge() < 60)
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
+        System.out.println(performance);
+
+
+
     }
 }
